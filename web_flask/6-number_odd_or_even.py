45b95af26c1a7580eb84a5dc3dd679cb385e7384
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-14 14:59:41 +0200

    task 6 pcs

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,10 +44,12 @@ def number_body(n):
     """change a variable inside a thml body"""
     return render_template("5-number.html", n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     """check if the number is odd or even"""
     return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == '__main__':
     app.run()
