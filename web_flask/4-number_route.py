cf9aeeca6bffaafa2dbc246e392d0bddf04f581d
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-14 14:17:49 +0200

    task4 is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""import flask and use it"""
+from flask import Flask
+"""open the applicatation"""
+app = Flask(__name__)
+
+"""decorator"""
+
+
+@app.route('/', strict_slashes=False)
+def route():
+    """return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def routehbnb():
+    """return hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def route_c(text):
+    """return a value"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def route_python(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+"""verify if n is a number"""
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run()
